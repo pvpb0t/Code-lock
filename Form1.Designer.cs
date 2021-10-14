a0377adf07fe6323c9c6c1792017b32c13e73c3d
pvpb0t <74259011+pvpb0t@users.noreply.github.com>
2021-10-14 08:16:16 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace if7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 71);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(118, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 71);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(199, 177);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 71);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(280, 177);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 71);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(118, 254);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 71);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(199, 254);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 71);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(280, 254);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 71);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(199, 331);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 71);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(465, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 71);
+            this.label1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(465, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(290, 216);
+            this.label2.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(492, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 74);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Locked";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(496, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 41);
+            this.label4.TabIndex = 13;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(280, 331);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 71);
+            this.buttonReset.TabIndex = 14;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Keylock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        string Wcode = "[Inser code]";
+        string code;
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            code += "1";
+            label4.Text = code;
+            int drake = code.Length;
+
+
+            if ((drake == 4) && (code == Wcode))
+            {
+                label4.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label1.BackColor = Color.Green;
+                label3.Text = "Correct";
+            }else if (drake > 4)
+            {
+                label4.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                label1.BackColor = Color.Red;
+                code = String.Empty;
+                label4.Text = String.Empty;
+                label3.Text = "Locked";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            code += "2";
+            label4.Text = code;
+            int drake = code.Length;
+
+
+            if ((drake == 4) && (code == Wcode))
+            {
+                label4.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label1.BackColor = Color.Green;
+                label3.Text = "Correct";
+            }
+            else if (drake > 4)
+            {
+                label4.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                label1.BackColor = Color.Red;
+                code = String.Empty;
+                label4.Text = String.Empty;
+                label3.Text = "Locked";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            code += "3";
+            label4.Text = code;
+            int drake = code.Length;
+
+
+            if ((drake == 4) && (code == Wcode))
+            {
+                label4.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label1.BackColor = Color.Green;
+                label3.Text = "Correct";
+            }
+            else if (drake > 4)
+            {
+                label4.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                label1.BackColor = Color.Red;
+                code = String.Empty;
+                label4.Text = String.Empty;
+                label3.Text = "Locked";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+            code += "4";
+            label4.Text = code;
+            int drake = code.Length;
+
+
+            if ((drake == 4) && (code == Wcode))
+            {
+                label4.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label1.BackColor = Color.Green;
+                label3.Text = "Correct";
+            }
+            else if (drake > 4)
+            {
+                label4.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                label1.BackColor = Color.Red;
+                code = String.Empty;
+                label4.Text = String.Empty;
+                label3.Text = "Locked";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            code += "5";
+            label4.Text = code;
+            int drake = code.Length;
+
+
+            if ((drake == 4) && (code == Wcode))
+            {
+                label4.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label1.BackColor = Color.Green;
+                label3.Text = "Correct";
+            }
+            else if (drake > 4)
+            {
+                label4.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                label1.BackColor = Color.Red;
+                code = String.Empty;
+                label4.Text = String.Empty;
+                label3.Text = "Locked";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+
+            code += "6";
+            label4.Text = code;
+            int drake = code.Length;
+
+
+            if ((drake == 4) && (code == Wcode))
+            {
+                label4.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label1.BackColor = Color.Green;
+                label3.Text = "Correct";
+            }
+            else if (drake > 4)
+            {
+                label4.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                label1.BackColor = Color.Red;
+                code = String.Empty;
+                label4.Text = String.Empty;
+                label3.Text = "Locked";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+
+            code += "7";
+            label4.Text = code;
+            int drake = code.Length;
+
+
+            if ((drake == 4) && (code == Wcode))
+            {
+                label4.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label1.BackColor = Color.Green;
+                label3.Text = "Correct";
+            }
+            else if (drake > 4)
+            {
+                label4.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                label1.BackColor = Color.Red;
+                code = String.Empty;
+                label4.Text = String.Empty;
+                label3.Text = "Locked";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+
+            code += "8";
+            label4.Text = code;
+            int drake = code.Length;
+
+
+            if ((drake == 4) && (code == Wcode))
+            {
+                label4.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label1.BackColor = Color.Green;
+                label3.Text = "Correct";
+            }
+            else if (drake > 4)
+            {
+                label4.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                label1.BackColor = Color.Red;
+                code = String.Empty;
+                label4.Text = String.Empty;
+                label3.Text = "Locked";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+
+            code += "9";
+            label4.Text = code;
+            int drake = code.Length;
+
+
+            if ((drake == 4) && (code == Wcode))
+            {
+                label4.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label1.BackColor = Color.Green;
+                label3.Text = "Correct";
+            }
+            else if (drake > 4)
+            {
+                label4.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                label1.BackColor = Color.Red;
+                code = String.Empty;
+                label4.Text = String.Empty;
+                label3.Text = "Locked";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
